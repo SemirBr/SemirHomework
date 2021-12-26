bb5f8002c455299c005b77a9a1cfdf18a3e8eb39
Semir Brahimbashev <brahimbashev@abv.bg>
2021-12-26 15:58:13 +0200

    commit changes

---
+++ Ex-07.php
@@ -0,0 +1,38 @@
+<?php
+
+$n=5;
+  $value = 1;
+  for($i = 1; $i <= $n; $i++) 
+  {
+    for($j = 1; $j <= $i; $j++) {
+      echo "$value "; 
+      $value++;
+    }
+    echo "</br>"; 
+  }
+ 
+ echo "</br>";
+ 
+$n2=10;
+  $value2 = 1;
+  for($i = 1; $i <= $n2; $i++) 
+  {
+    for($j = 1; $j <= $i; $j++) {
+      echo "$value2 "; 
+      $value2++;
+    }
+    echo "</br>"; 
+  }
+  
+  echo "</br>";
+  
+  $n3=10;
+  $value3 = 1;
+  for($i = 1; $i <= $n3; $i++) 
+  {
+    for($j = 1; $j <= $i; $j++) {
+      echo "$value3 "; 
+      $value3++;
+    }
+    echo "</br>"; 
+  }
